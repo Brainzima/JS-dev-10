41429f59a9a7cdbc8b349bde1ae6edab5e6778b1
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-07-01 05:40:40 +0000

    js varibales & calculations

---
+++ 02_JS-variables&calculation/script.js
@@ -0,0 +1,53 @@
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+// console.log(50)
+
+// var number = 100
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+
+// var number = 100
+// type name = value
+
+// varibales name declration rule :
+// 1. variable name can not be starts with number
+// 2. varibale name can only starts with alphabets and underscore( _ )
+// 3. varibale name can not accept whitespace, it can be camelCase or underscore
+// 4. variable name can contain numbers 
+// 5. varibale name can not contain any special CharacterData such as symbols 
+
+// mobile_number = 7979786431
+// mobileNumber = 7979786431
+
+// number1 = 100
+// number2 = 200
+
+
+//***********Calculations********************** */
+
+var num1 = 500
+var num2 = 600
+var sum = num1 + num2
+console.log(sum)
+var sub = num2 - num1
+console.log(sub)
+var mul = num1 * num2
+console.log(mul)
+var div = num1 / num2
+console.log(div)
