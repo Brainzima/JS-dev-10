9c0cc263fe2d04c939d2c60f4ca2efabeac02d1d
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-07-02 05:29:59 +0000

    js- let, const, var

---
+++ 03_JS-let-const-var/script.js
@@ -0,0 +1,40 @@
+// var number = 500
+// console.log(number)
+
+// let numberl = 1000
+// console.log(numberl)
+
+// const numberc = 5000
+// console.log(numberc)
+
+
+// Var - global scope,  redeclare , reassign
+// var declaration
+// var number = 100
+// console.log(number);
+//redeclaration
+// var number = 500
+// console.log(number)
+// reassign
+// number = 1000
+// console.log(number)
+
+
+// Let  -  block scope , reassign
+//declaration
+// let number = 50
+// console.log(number)
+// can not be redeclared
+
+//reassign
+// number = 40
+// console.log(number)
+
+
+// Const - block scope, 
+// declaration 
+const number = 500
+console.log(number)
+//can not be redeclared
+
+// can not be reassigned
