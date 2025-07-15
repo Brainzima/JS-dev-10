5c046d8e9b2b5dca76519391fba49b69e664ac1e
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-07-15 05:55:04 +0000

    js calculation arith...

---
+++ 05_JS-SnippetEX/script.js
@@ -0,0 +1,5 @@
+document.write("This text is from external js")
+
+const card = '<div class="card"> CARD </div>'
+
+document.writeln(card)
---
+++ 06_JS-Calculations/script.js
@@ -0,0 +1,35 @@
+let num1 = 1500
+let num2 = 400
+
+// add both numbers
+let add = num1 + num2
+
+//long way
+// document.writeln("Number1 = ")
+// document.writeln(num1)
+
+// document.writeln("Number2 = ")
+// document.writeln(num2)
+
+// document.writeln("Sum of both numbers = ")
+// document.writeln(add)
+
+//short way-1
+// document.writeln("Number1="+num1+" Number2="+num2+" Sum of both="+add)
+
+// short way-2 
+document.writeln(`Number1=${num1} Number2=${num2} Sum of both=${add}`)
+
+//subtraction of both numbers
+let sub = num1 - num2
+document.writeln(`<br>Number1=${num1} Number2=${num2} Subtration of both=${sub}`)
+
+//multiplication
+let mul = num1 * num2
+document.writeln(`<br>Number1=${num1} Number2=${num2} Multiplication of both=${mul}`)
+
+//divsion
+let div = num1 / num2 
+document.writeln(`<br>Number1=${num1} Number2=${num2} Division of both=${div}`)
+
+
