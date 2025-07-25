3f5c94ca1449ff0021ddf5b9ea914449abc50d5a
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-07-25 05:53:07 +0000

    area and perimeter of circle, rectangle, square and triangle

---
+++ 12_JS-CS-HW2/script.js
@@ -0,0 +1,17 @@
+//take a variable for radius and find the area, perimeter of circle
+let r = 44
+const pie = 3.14
+let area = pie * r * r
+let perimeter = 2 * pie * r
+document.writeln(`
+    Radius = ${r}<br>
+    Area of Circle= ${area}<br>
+    Perimeter of circle=${perimeter}<br>
+    `)
+
+
+// take the height and width of rectangle and find its area and perimeter
+
+// take the side of square and find its area and perimeter
+
+// take the width & height of triangle and find its area and perimeter
