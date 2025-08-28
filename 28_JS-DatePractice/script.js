17aead172dd2185453f224dbfc5a16a442ca94ed
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-08-28 06:09:02 +0000

    string methods in js

---
+++ 28_JS-DatePractice/script.js
@@ -0,0 +1,8 @@
+let cd = new Date()
+let ct = cd.getMinutes()
+// console.log(ct)
+if (ct >= 45) {
+    console.log("Form filing is now closed.")
+}else{
+    console.log("Form is filling fast ... file your form now...")
+}
---
+++ 29_JS-StringMethods1/script.js
@@ -0,0 +1,19 @@
+let txt = "BRaINZIMA"
+
+console.log(txt)
+console.log(txt.at(0))
+console.log(txt.at(5))
+console.log(txt.charAt(5))
+
+// to print the ascii value that character
+console.log(txt.codePointAt(2))
+console.log(txt.charCodeAt(2))
+
+console.log(txt.length)
+
+console.log(txt.indexOf("Z"))
+
+console.log(txt.endsWith("M"))
+console.log(txt.endsWith("A"))
+
+console.log(txt.search("GHFH"))
