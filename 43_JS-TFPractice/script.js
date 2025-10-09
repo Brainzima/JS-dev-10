fe2310d4f41f8087447d4b7828ebf6833cb1d7ca
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-10-09 05:43:54 +0000

    diwali lights basic concept

---
+++ 43_JS-TFPractice/script.js
@@ -0,0 +1,124 @@
+// function diyaJalao() {
+    
+// }
+
+setInterval(() => {
+    document.getElementById("diya1").style.backgroundColor="red"
+    document.getElementById("diya2").style.backgroundColor="blue"
+    document.getElementById("diya3").style.backgroundColor="orange"
+    document.getElementById("diya4").style.backgroundColor="green"
+    document.getElementById("diya5").style.backgroundColor="yellow"
+    document.getElementById("diya6").style.backgroundColor="pink"
+    document.getElementById("diya7").style.backgroundColor="purple"
+    document.getElementById("diya8").style.backgroundColor="navy"
+    document.getElementById("diya9").style.backgroundColor="white"
+    document.getElementById("diya10").style.backgroundColor="lightblue"
+}, 100);
+setInterval(() => {
+    document.getElementById("diya2").style.backgroundColor="red"
+    document.getElementById("diya3").style.backgroundColor="blue"
+    document.getElementById("diya4").style.backgroundColor="orange"
+    document.getElementById("diya5").style.backgroundColor="green"
+    document.getElementById("diya6").style.backgroundColor="yellow"
+    document.getElementById("diya7").style.backgroundColor="pink"
+    document.getElementById("diya8").style.backgroundColor="purple"
+    document.getElementById("diya9").style.backgroundColor="navy"
+    document.getElementById("diya10").style.backgroundColor="white"
+    document.getElementById("diya1").style.backgroundColor="lightblue"
+}, 200);
+setInterval(() => {
+    document.getElementById("diya3").style.backgroundColor="red"
+    document.getElementById("diya4").style.backgroundColor="blue"
+    document.getElementById("diya5").style.backgroundColor="orange"
+    document.getElementById("diya6").style.backgroundColor="green"
+    document.getElementById("diya7").style.backgroundColor="yellow"
+    document.getElementById("diya8").style.backgroundColor="pink"
+    document.getElementById("diya9").style.backgroundColor="purple"
+    document.getElementById("diya10").style.backgroundColor="navy"
+    document.getElementById("diya1").style.backgroundColor="white"
+    document.getElementById("diya2").style.backgroundColor="lightblue"
+}, 300);
+setInterval(() => {
+    document.getElementById("diya4").style.backgroundColor="red"
+    document.getElementById("diya5").style.backgroundColor="blue"
+    document.getElementById("diya6").style.backgroundColor="orange"
+    document.getElementById("diya7").style.backgroundColor="green"
+    document.getElementById("diya8").style.backgroundColor="yellow"
+    document.getElementById("diya9").style.backgroundColor="pink"
+    document.getElementById("diya10").style.backgroundColor="purple"
+    document.getElementById("diya1").style.backgroundColor="navy"
+    document.getElementById("diya2").style.backgroundColor="white"
+    document.getElementById("diya3").style.backgroundColor="lightblue"
+}, 400);
+setInterval(() => {
+    document.getElementById("diya5").style.backgroundColor="red"
+    document.getElementById("diya6").style.backgroundColor="blue"
+    document.getElementById("diya7").style.backgroundColor="orange"
+    document.getElementById("diya8").style.backgroundColor="green"
+    document.getElementById("diya9").style.backgroundColor="yellow"
+    document.getElementById("diya10").style.backgroundColor="pink"
+    document.getElementById("diya1").style.backgroundColor="purple"
+    document.getElementById("diya2").style.backgroundColor="navy"
+    document.getElementById("diya3").style.backgroundColor="white"
+    document.getElementById("diya4").style.backgroundColor="lightblue"
+}, 500);
+setInterval(() => {
+    document.getElementById("diya6").style.backgroundColor="red"
+    document.getElementById("diya7").style.backgroundColor="blue"
+    document.getElementById("diya8").style.backgroundColor="orange"
+    document.getElementById("diya9").style.backgroundColor="green"
+    document.getElementById("diya10").style.backgroundColor="yellow"
+    document.getElementById("diya1").style.backgroundColor="pink"
+    document.getElementById("diya2").style.backgroundColor="purple"
+    document.getElementById("diya3").style.backgroundColor="navy"
+    document.getElementById("diya4").style.backgroundColor="white"
+    document.getElementById("diya5").style.backgroundColor="lightblue"
+}, 600);
+setInterval(() => {
+    document.getElementById("diya7").style.backgroundColor="red"
+    document.getElementById("diya8").style.backgroundColor="blue"
+    document.getElementById("diya9").style.backgroundColor="orange"
+    document.getElementById("diya10").style.backgroundColor="green"
+    document.getElementById("diya1").style.backgroundColor="yellow"
+    document.getElementById("diya2").style.backgroundColor="pink"
+    document.getElementById("diya3").style.backgroundColor="purple"
+    document.getElementById("diya4").style.backgroundColor="navy"
+    document.getElementById("diya5").style.backgroundColor="white"
+    document.getElementById("diya6").style.backgroundColor="lightblue"
+}, 700);
+setInterval(() => {
+    document.getElementById("diya8").style.backgroundColor="red"
+    document.getElementById("diya9").style.backgroundColor="blue"
+    document.getElementById("diya10").style.backgroundColor="orange"
+    document.getElementById("diya1").style.backgroundColor="green"
+    document.getElementById("diya2").style.backgroundColor="yellow"
+    document.getElementById("diya3").style.backgroundColor="pink"
+    document.getElementById("diya4").style.backgroundColor="purple"
+    document.getElementById("diya5").style.backgroundColor="navy"
+    document.getElementById("diya6").style.backgroundColor="white"
+    document.getElementById("diya7").style.backgroundColor="lightblue"
+}, 800);
+setInterval(() => {
+    document.getElementById("diya9").style.backgroundColor="red"
+    document.getElementById("diya10").style.backgroundColor="blue"
+    document.getElementById("diya1").style.backgroundColor="orange"
+    document.getElementById("diya2").style.backgroundColor="green"
+    document.getElementById("diya3").style.backgroundColor="yellow"
+    document.getElementById("diya4").style.backgroundColor="pink"
+    document.getElementById("diya5").style.backgroundColor="purple"
+    document.getElementById("diya6").style.backgroundColor="navy"
+    document.getElementById("diya7").style.backgroundColor="white"
+    document.getElementById("diya8").style.backgroundColor="lightblue"
+}, 900);
+setInterval(() => {
+    document.getElementById("diya10").style.backgroundColor="red"
+    document.getElementById("diya1").style.backgroundColor="blue"
+    document.getElementById("diya2").style.backgroundColor="orange"
+    document.getElementById("diya3").style.backgroundColor="green"
+    document.getElementById("diya4").style.backgroundColor="yellow"
+    document.getElementById("diya5").style.backgroundColor="pink"
+    document.getElementById("diya6").style.backgroundColor="purple"
+    document.getElementById("diya7").style.backgroundColor="navy"
+    document.getElementById("diya8").style.backgroundColor="white"
+    document.getElementById("diya9").style.backgroundColor="lightblue"
+}, 1000);
